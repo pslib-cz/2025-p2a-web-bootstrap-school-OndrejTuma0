0a447d0acbfb075bf125534ae01b63d5312ee2c8
unknown <ondrej.tuma.024@pslib.cz>
2025-09-05 12:54:20 +0200

    update ...

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
